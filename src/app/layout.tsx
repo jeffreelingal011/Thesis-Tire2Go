0783c301f4be7b531a476f79ead950ac292867dc
ricajunsay <ricayvonne.junsay@tup.edu.ph>
2025-09-22 21:02:35 +0800

    app
    finish 30% here

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import "@/lib/cron";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Tire2Go - Tire and Mags Collection",
+  description:
+    "A collection of 202 magazines and tire catalogs from the 20th century.",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning suppressContentEditableWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+          <Toaster position="bottom-right" />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
