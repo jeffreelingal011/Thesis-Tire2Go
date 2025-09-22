0f7e99d54d345186d9670e6d1f58bdcd38ef930a
ricajunsay <ricayvonne.junsay@tup.edu.ph>
2025-09-22 21:23:25 +0800

    app update
    nadoble

--- src/actions/app/api/_cron-init/route.ts
+++
@@ -1,7 +0,0 @@
-import "@/lib/cron"; // 👈 just importing runs the scheduler
-
-export async function GET() {
-  return Response.json({
-    message: "Cron initialized and running in background.",
-  });
-}
