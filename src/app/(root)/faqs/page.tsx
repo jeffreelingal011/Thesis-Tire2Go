79d574f2fb63fab3d016738bc4a7b7285d4be6b9
jeffreelingal011 <145845016+jeffreelingal011@users.noreply.github.com>
2025-11-12 17:36:18 +0800

    finish revision.

--- src/app/(root)/faqs/page.tsx
+++ src/app/(root)/faqs/page.tsx
@@ -16,9 +16,9 @@ const Page = async () => {
   });
   return (
     <div className="min-h-screen bg-[#f5f5f5]">
-      <div className="pt-30 px-24">
+      <div className="pt-40 px-24">
         <h3 className="text-black text-4xl font-bold tracking-tight text-center">
-          Tire2Go Frequently Asked Questions
+          Tyre2Go Frequently Asked Questions
         </h3>
         <Separator className="mt-5 mb-10 bg-primary rouned-lg !h-[5px]" />
         <Accordion type="single" collapsible className="my-4 w-full space-y-2">
@@ -34,11 +34,11 @@ const Page = async () => {
               <AccordionContent>
                 <div
                   className="prose prose-md max-w-none
-           prose-headings:font-bold
-           prose-headings:text-muted-foreground
-           prose-a:text-primary prose-a:underline
-           prose-ul:list-disc prose-ol:list-decimal
-           prose-li:marker:text-muted-foreground prose-img:w-[400px] prose-img:h-[200px] prose-img:object-contain text-muted-foreground"
+                  prose-headings:font-bold
+                  prose-headings:text-muted-foreground
+                  prose-a:text-primary prose-a:underline
+                  prose-ul:list-disc prose-ol:list-decimal
+                  prose-li:marker:text-muted-foreground prose-img:w-[400px] prose-img:h-[200px] prose-img:object-contain text-muted-foreground"
                   dangerouslySetInnerHTML={{ __html: item.answer }}
                 />
               </AccordionContent>
